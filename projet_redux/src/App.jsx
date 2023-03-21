0bc08103d1536657428ee3ae6a4b150b6f740c1d
55973 <55973@etu.he2b.be>
2023-03-21 09:56:11 +0100

    first commit

---
+++ projet_redux/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-red-500 underline-offset-4 font-light underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
