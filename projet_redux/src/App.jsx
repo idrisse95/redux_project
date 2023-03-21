27a38aeb0e5ac124b4a66b68183ac772090a5b2e
55973 <55973@etu.he2b.be>
2023-03-21 10:21:38 +0100

    commit1

--- projet_redux/src/App.jsx
+++ projet_redux/src/App.jsx
@@ -1,12 +1,12 @@
 
-import './App.css'
+import './App.scss'
 
 function App() {
  
 
   return (
     <div className="App">
-      <h1 className="text-3xl text-red-500 underline-offset-4 font-light underline">
+      <h1 className=" text-red-500 underline-offset-4 font-light underline">
         Hello world!
       </h1>
     </div>
